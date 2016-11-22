a2e0f9cb9b71de91a5ada4e4c32841b3ad5f44b2
christopher LONG <christopher LONG>
2016-11-22 10:58:43 +0100

    Create classe LoginServiceTest

---
+++ src/test/java/geco/LoginServiceTest.java
@@ -0,0 +1,34 @@
+package geco;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by christopher on 22/11/2016.
+ */
+public class LoginServiceTest extends TestCase {
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testLoginExists() throws Exception {
+
+    }
+
+    public void testAddLogin() throws Exception {
+
+    }
+
+    public void testFindAllLoginsStartingWith() throws Exception {
+
+    }
+
+    public void testFindAllLogins() throws Exception {
+
+    }
+
+}

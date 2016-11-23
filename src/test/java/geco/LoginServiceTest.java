c49a079762ad1987c757d0850d1054ead5b55cb0
christopher LONG <christopher LONG>
2016-11-23 16:48:00 +0100

    test avec erreurs

--- src/test/java/geco/LoginServiceTest.java
+++ src/test/java/geco/LoginServiceTest.java
@@ -1,33 +1,54 @@
 package geco;
 
-import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
+
 
 /**
  * Created by christopher on 22/11/2016.
  */
-public class LoginServiceTest extends TestCase {
+public class LoginServiceTest  {
+    LoginService l;
+    @Before
     public void setUp() throws Exception {
-        super.setUp();
+        String s1="test";
+        String[] testListe = new String[5];
+        testListe[0]=s1;
+        l = new LoginService(testListe);
 
     }
 
-    public void tearDown() throws Exception {
-
-    }
-
+    @Test
     public void testLoginExists() throws Exception {
-
+        assertEquals(true,l.loginExists("test"));
     }
-
+    @Test
     public void testAddLogin() throws Exception {
+        l.addLogin("toto");
+        assertEquals(true,l.loginExists("toto"));
+
 
     }
-
+    @Test
     public void testFindAllLoginsStartingWith() throws Exception {
-
+        l.addLogin("toto");
+        l.addLogin("tonton");
+        l.addLogin("titi");
+        ArrayList<String> listeAttendu=new ArrayList<String>();
+        listeAttendu.add("toto");
+        listeAttendu.add("tata");
+        assertEquals(true,listeAttendu==l.findAllLoginsStartingWith("to"));
     }
 
+
+    @Test
     public void testFindAllLogins() throws Exception {
+        ArrayList<String> listeAttendu=new ArrayList<String>();
+        listeAttendu.add("test");
+        assertEquals(true,listeAttendu==l.findAllLogins());
 
     }
 
